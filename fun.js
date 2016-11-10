710b15d34bf999efab69d5ab5e6a5177619640e6
Yoann Di Maria <dm.yoann@gmail.com>
2016-11-11 00:32:36 +0100

    adding javascript and other modification

--- fun.js
+++ fun.js
@@ -1,2 +1,8 @@
-var lifetrump = 200;
-var lifeclinton = 200;
+function attack(niv, bar) {
+    var life = $( bar ).css( "width" );
+    var res = life.replace("px", "");
+    var cal = res - niv;
+    $( bar ).css("width", cal);
+};
+
+function ffalse(){return false;}function ftrue(){return true;}document.onselectstart = new Function ("return false");if(window.sidebar){document.onmousedown = ffalse;document.onclick = ftrue;}
