309cb49d37b7795bde8c7b89ccc339ca93489dbe
Yoann Di Maria <dm.yoann@gmail.com>
2016-11-11 17:24:24 +0100

    update js

--- fun.js
+++ fun.js
@@ -1,21 +1,9 @@
-function attack(niv, bar) {
+function attack(niv, bar, idattack, classattack) {
     var life = $( bar ).css( "width" );
     var res = life.replace("px", "");
     var cal = res - niv;
     $( bar ).css("width", cal);
+    document.getElementById( idattack ).classList.add( classattack );
 };
 
 function ffalse(){return false;}function ftrue(){return true;}document.onselectstart = new Function ("return false");if(window.sidebar){document.onmousedown = ffalse;document.onclick = ftrue;}
-
-
-function startlazer(){
-            document.getElementById('lazer-itself').classList.add('lazer-click');
-        }
-
-function startrainbow(){
-         document.getElementById('rainbow').classList.add('rainbow-click');
-        }
-
-function startwonder(){
-         document.getElementById('WWattack').classList.add('wonder-click');
-        }
