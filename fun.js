4f6226968940e52a99dbd6388bcaaaf82eecbc13
Yoann Di Maria <dm.yoann@gmail.com>
2016-11-13 11:31:11 +0100

    edit

--- fun.js
+++ fun.js
@@ -1,9 +1,9 @@
 function attack(niv, bar, idattack, classattack) {
     var life = $( bar ).css( "width" );
-    var res = life.replace("px", "");
+    var res = life.replace( "px", "" );
     var cal = res - niv;
-    $( bar ).css("width", cal);
+    $( bar ).css( "width", cal );
     document.getElementById( idattack ).classList.add( classattack );
 };
 
-function ffalse(){return false;}function ftrue(){return true;}document.onselectstart = new Function ("return false");if(window.sidebar){document.onmousedown = ffalse;document.onclick = ftrue;}
+function ffalse(){return false;}function ftrue(){return true;}document.onselectstart=new Function("return false");if(window.sidebar){document.onmousedown=ffalse;document.onclick = ftrue;}
